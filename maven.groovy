ed97dbfc38af31d8c6e48d2b44916629c5b2c050
iamtruptimane <iamtruptimane@gmail.com>
2025-02-08 12:48:50 +0530

    added path

--- maven.groovy
+++ maven.groovy
@@ -8,7 +8,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                sh 'mvn clean package'
+                sh '/opt/apache-maven/bin mvn clean package'
             }
         }
         stage('Test') {
